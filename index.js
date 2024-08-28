2320c6116e4408a80d655b5878d57c4dbebdb942
Anirban Singha <143536290+SinghaAnirban005@users.noreply.github.com>
2024-08-28 18:08:14 +0000

    FIX: bug in index on adding todo

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ import fs from "fs"
 const program = new Command()
 
 let todos = [
- 
+
 ]
 
 if(fs.existsSync('todos.json')){
     const dataSet = fs.readFileSync("todos.json", 'utf-8')
-    if(dataSet){
+    if(dataSet) {
         todos = JSON.parse(dataSet)
     }
 }
+
+// section to enable adding todo in the array
 
 program
     .name("index")
@@ -25,15 +27,53 @@ program
     .argument('<string>', "Todo to add")
     .action((str) => {
 
-        todos.push(str)
+        const work = {
+            id: todos.length == 0 ? 0 : todos.length,
+            todo: str
+        }
+
+        todos.push(work)
+     
 
         fs.writeFile('todos.json', JSON.stringify(todos), (err) => {
+
             if(err) {
                 console.log(err)
                 throw err
             }
 
+            console.log(todos)
             console.log('Todo has been added')
+        })
+    })
+
+// section to delete todo ...
+program
+    .command('delete-todo')
+    .description('delete todo from file')
+    .argument('<string>', 'Todo to delete')
+    .action((id) => {
+        fs.readFile('todos.json', 'utf-8', (err, data) => {
+            if(err){
+                throw err
+            }
+
+        const set = JSON.parse(data)
+        let  i = 0;
+        while(i < set.length){
+            if(set[i].id == id) {
+                break;
+            }
+            else{
+                i++;
+            }
+        }
+
+        if(i == set.length) {
+            throw new Error("No todo with such id exists !!")
+        }
+
+        console.log(set[i].id)
         })
     })
 
